9751aff0221257ded6ef6ee8000b50de99cb58b7
Jason Surguine <jaybot7@users.noreply.github.com>
2016-06-09 13:11:02 +0900

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ var config = {};
 config.db = {};
 
 config.db.host = 'jaybot:seven@ds011374.mlab.com:11374';
-config.db.name = 'url-shortener-microservice';
+config.db.name = 'url-short';
 
 module.exports = config;
